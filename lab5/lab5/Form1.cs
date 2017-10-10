de232dd09c075d589c0d7cb7542a0e3886fb242a
WillyForest <kinashchuksasha@gmail.com>
2017-10-10 16:36:17 +0300

    lab 5 first button + form2

---
+++ lab5/lab5/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.ShowDialog();
+        }
+    }
+    public class Faculty
+    {
+        string name;
+        int course;
+        List<string> groups = new List<string>();
+        public Faculty(string name, int course, List<string> groups)
+        {
+            this.name = name;
+            this.course = course;
+            foreach (string group in groups)
+            {
+                this.groups.Add(group);
+            }
+        }
+    }
+    public class XPredmetList
+    {
+        public static string[] GetNames()
+        {
+            return items.ToArray();
+        }
+
+        static List<string> items;
+
+        // конструктор класса, кот. устанавливает список предметов
+        static XPredmetList()
+        {
+            items.AddRange(new string[] { "p1", "p2", "p3" });
+        }
+    }
+    public class XMark
+    {
+        //  код предмета
+        int predmetCode;
+        //  оценка по предмету
+        int mark;
+        //  дата сдачи
+        DateTime examDate;
+        // метод получения названия предмета
+        public string GetPredmetName()
+        {
+            return XPredmetList.GetNames()[this.predmetCode];
+        }
+    }
+    public class Student
+    {
+        int studentID;
+        // Имя студента
+        string firstName;
+        // Фамилия студента
+        string surName;
+        // Оценки данного студента
+        List<XMark> exams = new List<XMark>();
+    }
+
+
+
+}
---
+++ lab5/lab5/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form2 : Form
+    {
+        List<Faculty> facs = new List<Faculty>();
+        public Form2()
+        {
+            InitializeComponent();
+            comboBox2.Items.Add('1');
+            comboBox2.Items.Add('2');
+            comboBox2.Items.Add('3');
+            comboBox2.Items.Add('4');
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox3.Items.Add(textBox2.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<string> tGroups = new List<string>();
+            foreach(string cb in comboBox3.Items)
+            {
+                tGroups.Add(cb);
+            }
+            Faculty fac = new Faculty(comboBox1.SelectedItem.ToString(), 
+                Int32.Parse(comboBox2.SelectedItem.ToString()), tGroups);
+            facs.Add(fac);
+            Hide();
+        }
+    }
+}
