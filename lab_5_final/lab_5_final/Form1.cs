cad6e45f921026c2eba5177cc7f4eb8b7e83f820
WillyForest <kinashchuksasha@gmail.com>
2017-10-25 01:33:06 +0300

    lab5 FINISH

--- lab_5_final/lab_5_final/Form1.cs
+++ lab_5_final/lab_5_final/Form1.cs
@@ -22,6 +22,57 @@ namespace lab_5_final
             readFromFile();
         }
 
+        public void writeToFile()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("xmltext.xml");
+            //объявляем необходимые переменные
+            XmlElement xRoot = xDoc.DocumentElement;
+            xRoot.RemoveAll();
+            foreach (Student stud in students)
+            {
+                XmlElement studentElem = xDoc.CreateElement("student");
+                XmlElement courseElem = xDoc.CreateElement("course");
+                XmlElement facElem = xDoc.CreateElement("faculty");
+                XmlElement groupElem = xDoc.CreateElement("group");
+                XmlElement studentID = xDoc.CreateElement("ID");
+                XmlElement fNameElem = xDoc.CreateElement("firstName");
+                XmlElement sNameElem = xDoc.CreateElement("surName");
+                XmlElement marks = xDoc.CreateElement("marks");
+                //строим структуру хмл документа
+                
+                xRoot.AppendChild(studentElem);
+                studentElem.AppendChild(facElem);
+                studentElem.AppendChild(courseElem);
+                studentElem.AppendChild(groupElem);
+                studentElem.AppendChild(studentID);
+                studentElem.AppendChild(sNameElem);
+                studentElem.AppendChild(fNameElem);
+                studentElem.AppendChild(marks);
+                foreach (XMark exam in stud.exams)
+                {
+                    XmlElement predmet = xDoc.CreateElement("predmet");
+                    XmlElement code = xDoc.CreateElement("code");
+                    XmlElement mark = xDoc.CreateElement("mark");
+                    XmlElement examDate = xDoc.CreateElement("examDate");
+                    marks.AppendChild(predmet);
+                    predmet.AppendChild(code);
+                    predmet.AppendChild(mark);
+                    predmet.AppendChild(examDate);
+                    code.AppendChild(xDoc.CreateTextNode(exam.predmetCode + ""));
+                    mark.AppendChild(xDoc.CreateTextNode(exam.mark + ""));
+                    examDate.AppendChild(xDoc.CreateTextNode(exam.examDate + ""));
+                }
+                courseElem.AppendChild(xDoc.CreateTextNode(stud.course + ""));
+                facElem.AppendChild(xDoc.CreateTextNode(stud.fac.name + ""));
+                groupElem.AppendChild(xDoc.CreateTextNode(stud.group.name + ""));
+                studentID.AppendChild(xDoc.CreateTextNode(stud.studentID + ""));
+                fNameElem.AppendChild(xDoc.CreateTextNode(stud.firstName));
+                sNameElem.AppendChild(xDoc.CreateTextNode(stud.surName));
+            }
+            xDoc.Save("xmltext.xml");
+        }
+
         public void readFromFile()
         {
             XmlDocument xDoc = new XmlDocument();
@@ -31,18 +82,60 @@ namespace lab_5_final
             if (xRoot.HasChildNodes)
             {
                 //находим все факультеты, которые есть в xml файле
-                foreach (XmlElement s in xDoc.GetElementsByTagName("faculty"))
+                foreach (XmlElement facult in xDoc.GetElementsByTagName("faculty"))
                 {
                     try
                     {
-                        if (facs.Contains(facs.Where(f => f.name == s.InnerText).First()))
+                        if (facs.Contains(facs.Where(f => f.name == facult.InnerText).First()))
                         {
                             continue; //если факультет уже есть в списке, пропускаем, если нет - ловим ошибку и добавляем его
                         }
                     } catch
                     {
-                        Faculty fac = new Faculty(s.InnerText);
+                        Faculty fac = new Faculty(facult.InnerText);
                         facs.Add(fac);
+                    }
+                }
+                foreach (XmlElement gr in xDoc.GetElementsByTagName("group"))
+                {
+                    try
+                    {
+                        if (groups.Contains(groups.Where(g => g.name == gr.InnerText).First()))
+                        {
+                            continue; //если группа уже есть в списке, пропускаем, если нет - 
+                                        //ловим ошибку и добавляем еe
+                        }
+                    }
+                    catch
+                    {
+                        Group group = new Group(gr.InnerText);
+                        groups.Add(group);
+                    }
+                }
+                foreach (XmlElement s in xDoc.GetElementsByTagName("student"))
+                {
+                    try
+                    {
+                        if (students.Contains(students.Where(st => st.studentID == int.Parse(s.GetElementsByTagName("ID")[0].InnerText)).First()))
+                        {
+                            continue;
+                        }
+                    } catch
+                    {
+                        List<XMark> exams = new List<XMark>();
+                        foreach (XmlElement mrk in s.GetElementsByTagName("predmet"))
+                        {
+                            XMark mark = new XMark(int.Parse(mrk.GetElementsByTagName("code")[0].InnerText), 
+                                int.Parse(mrk.GetElementsByTagName("mark")[0].InnerText), 
+                                DateTime.Parse(mrk.GetElementsByTagName("examDate")[0].InnerText));
+                            exams.Add(mark);
+                        }
+                        Student stud = new Student(s.GetElementsByTagName("firstName")[0].InnerText, 
+                            s.GetElementsByTagName("surName")[0].InnerText, exams, 
+                            facs.Where(f => f.name == s.GetElementsByTagName("faculty")[0].InnerText).First(), 
+                            groups.Where(g => g.name == s.GetElementsByTagName("group")[0].InnerText).First(), 
+                            int.Parse(s.GetElementsByTagName("course")[0].InnerText));
+                        students.Add(stud);
                     }
                 }
             }
@@ -80,7 +173,118 @@ namespace lab_5_final
 
         private void button6_Click(object sender, EventArgs e)
         {
+            writeToFile();
             Application.Exit();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            personalTask();
+        }
+
+        public void personalTask()
+        {
+            
+            List<Student> bestStudents = new List<Student>();
+            foreach (Student student in students)
+            {
+                double smark = 0;
+                foreach (XMark mark in student.exams)
+                {
+                    smark += mark.mark;
+                }
+                smark = smark / student.exams.Count;
+                if (smark == 4.5)
+                {
+                    bestStudents.Add(student);
+                }
+            }
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("answer.xml");
+            //объявляем необходимые переменные
+            XmlElement xRoot = xDoc.DocumentElement;
+            xRoot.RemoveAll();
+            foreach (Student student in bestStudents) //вариант где отобразим только то что в задании
+            {
+                XmlElement studentElem = xDoc.CreateElement("student");
+                XmlElement courseElem = xDoc.CreateElement("course");
+                XmlElement facElem = xDoc.CreateElement("faculty");
+                XmlElement groupElem = xDoc.CreateElement("group");
+                XmlElement studentID = xDoc.CreateElement("ID");
+                XmlElement fNameElem = xDoc.CreateElement("firstName");
+                XmlElement sNameElem = xDoc.CreateElement("surName");
+                //строим структуру хмл документа
+
+                xRoot.AppendChild(studentElem);
+                studentElem.AppendChild(facElem);
+                studentElem.AppendChild(courseElem);
+                studentElem.AppendChild(groupElem);
+                studentElem.AppendChild(studentID);
+                studentElem.AppendChild(sNameElem);
+                studentElem.AppendChild(fNameElem);
+                foreach (XMark exam in student.exams)
+                {
+                    XmlElement predmet = xDoc.CreateElement("predmet");
+                    XmlElement code = xDoc.CreateElement("code");
+                    XmlElement mark = xDoc.CreateElement("mark");
+                    XmlElement examDate = xDoc.CreateElement("examDate");
+                    predmet.AppendChild(code);
+                    predmet.AppendChild(mark);
+                    predmet.AppendChild(examDate);
+                    code.AppendChild(xDoc.CreateTextNode(exam.predmetCode + ""));
+                    mark.AppendChild(xDoc.CreateTextNode(exam.mark + ""));
+                    examDate.AppendChild(xDoc.CreateTextNode(exam.examDate + ""));
+                }
+                courseElem.AppendChild(xDoc.CreateTextNode(student.course + ""));
+                facElem.AppendChild(xDoc.CreateTextNode(student.fac.name + ""));
+                groupElem.AppendChild(xDoc.CreateTextNode(student.group.name + ""));
+                studentID.AppendChild(xDoc.CreateTextNode(student.studentID + ""));
+                fNameElem.AppendChild(xDoc.CreateTextNode(student.firstName));
+                sNameElem.AppendChild(xDoc.CreateTextNode(student.surName));
+
+            }
+            /*foreach (Student student in bestStudents) //вариант где отобразим всю информацию
+            {
+                XmlElement studentElem = xDoc.CreateElement("student");
+                XmlElement courseElem = xDoc.CreateElement("course");
+                XmlElement facElem = xDoc.CreateElement("faculty");
+                XmlElement groupElem = xDoc.CreateElement("group");
+                XmlElement studentID = xDoc.CreateElement("ID");
+                XmlElement fNameElem = xDoc.CreateElement("firstName");
+                XmlElement sNameElem = xDoc.CreateElement("surName");
+                XmlElement marks = xDoc.CreateElement("marks");
+                //строим структуру хмл документа
+
+                xRoot.AppendChild(studentElem);
+                studentElem.AppendChild(facElem);
+                studentElem.AppendChild(courseElem);
+                studentElem.AppendChild(groupElem);
+                studentElem.AppendChild(studentID);
+                studentElem.AppendChild(sNameElem);
+                studentElem.AppendChild(fNameElem);
+                studentElem.AppendChild(marks);
+                foreach (XMark exam in student.exams)
+                {
+                    XmlElement predmet = xDoc.CreateElement("predmet");
+                    XmlElement code = xDoc.CreateElement("code");
+                    XmlElement mark = xDoc.CreateElement("mark");
+                    XmlElement examDate = xDoc.CreateElement("examDate");
+                    marks.AppendChild(predmet);
+                    predmet.AppendChild(code);
+                    predmet.AppendChild(mark);
+                    predmet.AppendChild(examDate);
+                    code.AppendChild(xDoc.CreateTextNode(exam.predmetCode + ""));
+                    mark.AppendChild(xDoc.CreateTextNode(exam.mark + ""));
+                    examDate.AppendChild(xDoc.CreateTextNode(exam.examDate + ""));
+                }
+                courseElem.AppendChild(xDoc.CreateTextNode(student.course + ""));
+                facElem.AppendChild(xDoc.CreateTextNode(student.fac.name + ""));
+                groupElem.AppendChild(xDoc.CreateTextNode(student.group.name + ""));
+                studentID.AppendChild(xDoc.CreateTextNode(student.studentID + ""));
+                fNameElem.AppendChild(xDoc.CreateTextNode(student.firstName));
+                sNameElem.AppendChild(xDoc.CreateTextNode(student.surName));
+            }*/
+            xDoc.Save("answer.xml");
         }
     }
     public class Faculty
@@ -117,11 +321,11 @@ namespace lab_5_final
     public class XMark
     {
         //  код предмета
-        int predmetCode;
+        public int predmetCode;
         //  оценка по предмету
-        int mark;
+        public int mark;
         //  дата сдачи
-        DateTime examDate;
+        public DateTime examDate;
         // метод получения названия предмета
         public string GetPredmetName()
         {
